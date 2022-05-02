6a40caa4bc93231df302829f79dcf39ec493adc9
emily <emilyvicki601@gmail.com>
2022-05-02 20:53:13 +0800

    feat: display to months' view

---
+++ src/components/CalendarMonthsView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-6" @click="updateActiveMonth">
+    <span
+      :class="this.currentM === this.month.id ? 'text-white rounded-full bg-red-500 font-medium p-3' : ''"
+      class="cursor-pointer p-3 rounded-full hover:bg-red-500 hover:text-white"
+      >{{ this.month.name }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalendarMonthsView",
+  props: {
+    month: {
+      require: true,
+    },
+    currentM: {
+      type: String,
+    },
+  },
+  methods: {
+    updateActiveMonth() {
+      this.$emit("update-active-month", this.month.id);
+    },
+  },
+};
+</script>
