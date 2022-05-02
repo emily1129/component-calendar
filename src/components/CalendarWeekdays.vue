d192eddb534325e40ec2c1741c202ec4cc406174
emily <emilyvicki601@gmail.com>
2022-05-02 20:52:12 +0800

    feat: add weekdays ui

---
+++ src/components/CalendarWeekdays.vue
@@ -0,0 +1,17 @@
+<template>
+  <ol class="m-1 grid grid-cols-7 font-bold">
+    <li v-for="weekday in weekdays" :key="weekday">{{ weekday }}</li>
+  </ol>
+</template>
+
+<script>
+
+export default {
+  name: "CalendarWeekdays",
+  data() {
+    return {
+      weekdays: ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
+    }
+  }
+};
+</script>
